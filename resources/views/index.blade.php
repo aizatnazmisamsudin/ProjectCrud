55049ed0f72cb2388bf069f0d58e9d4f8de47ed8
aizatnazmisamsudin <aizatnazmisamsudin97@gmail.com>
2024-03-15 05:45:15 +0800

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,324 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <link
+      href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900"
+      rel="stylesheet"
+    />
+
+    <title>Aizat Nazmi</title>
+<!--
+Reflux Template
+https://templatemo.com/tm-531-reflux
+-->
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="assets/css/fontawesome.css" />
+    <link rel="stylesheet" href="assets/css/templatemo-style.css" />
+    <link rel="stylesheet" href="assets/css/owl.css" />
+    <link rel="stylesheet" href="assets/css/lightbox.css" />
+  </head>
+
+  <body>
+    <div id="page-wraper">
+      <!-- Sidebar Menu -->
+      <div class="responsive-nav">
+        <i class="fa fa-bars" id="menu-toggle"></i>
+        <div id="menu" class="menu">
+          <i class="fa fa-times" id="menu-close"></i>
+          <div class="container">
+            <div class="image">
+              <a href="#"><img src="assets/images/295.jpg" alt="" /></a>
+            </div>
+            <div class="author-content">
+              <h5>Mohd Aizat Nazmi</h5>
+              <span>Junior Laravel Developer</span>
+              <ul><span>aizatnazmisamsudin97@gmail.com</span></ul>
+            </div>
+            <nav class="main-nav" role="navigation">
+              <ul class="main-menu">
+                <li><a href="#section1">About Me</a></li>
+                <li><a href="#section2">What I’m good at</a></li>
+                <li><a href="#section3">My Work</a></li>
+                
+              </ul>
+            </nav>
+            <div class="social-network">
+              <ul class="soial-icons">
+                <li>
+                  <a href="https://www.facebook.com/aizatnazmiacai/"
+                    ><i class="fa fa-facebook"></i
+                  ></a>
+                </li>
+                <li>
+                  <a href="https://twitter.com/aizatnazmiacai">
+                    <i class="fa fa-twitter"></i></a>
+                </li>
+                <li>
+                  <a href="www.linkedin.com/in/mohd-aizat-nazmi">
+                    <i class="fa fa-linkedin"></i></a>
+                </li>
+                <li>
+              
+                </li>
+              </ul>
+            </div>
+            <div class="copyright-text">
+              <p></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <section class="section about-me" data-section="section1">
+        <div class="container">
+          <div class="section-heading">
+            <h2>About Me</h2>
+            <div class="line-dec"></div>
+            <span
+              >Adept at leveraging technology to enhance guest experiences and streamline operations.
+Skilled in client relationship management and IT solution implementation. Known for
+delivering tangible business outcomesthrough innovative technology strategies. Strong
+communicator and collaborator who excels in cross-functional teams. Ready to bring my
+expertise to drive excellence in your organization..</span
+            >
+          </div>
+          <div class="left-image-post">
+            <div class="row">
+              <div class="col-md-6">
+                <div class="left-image">
+                  <img src="assets/images/left-image.jpg" alt="" />
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="right-text">
+                  <h4>OLYRIN TECHNOLOGY SB</h4>
+                  <p>Business Development Manager.</p>
+                  <ul>(2023-Present)</ul>
+                  <p>
+                    <a><ul>Conducting market research to identify potential new markets,
+                    trends, and opportunities.</ul>
+                    <ul>Analyzing competitor activities and industry trends to stay ahead of
+                    the curve.</ul>
+                    <ul>Building and maintaining strong relationships with existing clients
+                    and partners.</ul></a>
+                  </p>  
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="right-image-post">
+            <div class="row">
+              <div class="col-md-6">
+                <div class="left-text">
+                  <h4>DAMAI BEACH RESORT & DAMAI LAGOON RESORT</h4>
+                  <p>IT Executive</p>
+                  <ul>(2022-2023)</ul>
+                  <p>
+                  <ul>To ensure the smooth operation oftechnology systems that enhance guest experiences, improve
+                      operational efficiency, and safeguard data and information.</ul>
+                  <ul>To oversee the design, implementation, and maintenance of the organization's IT infrastructure,
+                      including servers, networks, and hardware.</ul>
+                  <ul>To implement and manage guest-facing technology solutionssuch as reservation systems, inroomamenities, and guest Wi-Fi.</ul>
+                  </p>
+                  
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="right-image">
+                  <img src="assets/images/right-image.jpg" alt="" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="section my-services" data-section="section2">
+        <div class="container">
+          <div class="section-heading">
+            <h2>What I’m good at?</h2>
+            <div class="line-dec"></div>
+            <span
+              >I thrive in dynamic environments and capable to adapt quickly for new challenges. 
+              My strong communication skills enable me to collaborate effectively with teams , 
+              ensuring smooth project execution and fostering a positive work environment.</span
+            >
+          </div>
+          <div class="row">
+            <div class="col-md-6">
+              <div class="service-item">
+                <div class="first-service-icon service-icon"></div>
+                <h4>Management Skills</h4>
+                <p>
+                <p>Leadership</p>
+                <p>Communication</p>
+                <p>Decision Making</p>
+                <p>Problem-Solving</p>
+                <p>Time Management</p>
+                </p>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="service-item">
+                <div class="second-service-icon service-icon"></div>
+                <h4>Creative Ideas</h4>
+                <p>
+                <p>Clarity</p>
+                <p>Uniqueness</p>
+                <p>Benefits</p>
+                <p>Feasibility</p>
+                <p>Impact</p>
+                <p>Visualization</p>
+                </p>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="service-item">
+                <div class="third-service-icon service-icon"></div>
+                <h4>Personal Skill</h4>
+                <p>
+                <p>Office(Word,Powerpoint)</p>
+                <p>Network Troubleshoot</p>
+                <p>PC Troubleshoot</p>
+                <p>Canva,Adobe</p>  
+                </p>
+              </div>
+            </div>
+            
+          </div>
+        </div>
+      </section>
+
+      <section class="section my-work" data-section="section3">
+        <div class="container">
+          <div class="section-heading">
+            <h2>My Work</h2>
+            <div class="line-dec"></div>
+            </head>
+    <body>
+
+        <div class="container" style="margin-top: 50px;">
+        <style>
+              table, th, td {
+              border: 2px solid white;
+              }
+        </style>
+            <h3 class="text-center text-danger"><b>Laravel CRUD With Multiple Image Upload</b> </h3>
+            <p></p>
+            <a href="/create" class="btn btn-outline-success">Add New Post</a>
+            <div class="line-dec"></div>
+            <table class="table">
+                <thead>
+                  <tr>
+                    <th>Title</th>
+                    <th>Author</th>
+                    <th>Description</th>
+                    <th>Product Image</th>
+                    <th>Update</th>
+                    <th>Delete</th>
+                  </tr>
+                </thead>
+                <tbody>
+
+
+                    @foreach ($posts as $post)
+                 <tr>
+                       
+                       <td>{{ $post->title }}</td>
+                       <td>{{ $post->author }}</td>
+                       <td>{{ $post->body }}</td>
+                       <td><img src="cover/{{ $post->cover }}" class="img-responsive" style="max-height:100px; max-width:100px" alt="" srcset=""></td>
+                       <td><a href="/edit/{{ $post->id }}" class="btn btn-outline-primary">Update</a></td>
+                       <td>
+                           <form action="/delete/{{ $post->id }}" method="post">
+                            <button class="btn btn-outline-danger" onclick="return confirm('Are you sure?');" type="submit">Delete</button>
+                            @csrf
+                            @method('delete')
+                        </form>
+                       </td>
+
+                   </tr>
+                   @endforeach
+
+                </tbody>
+              </table>
+        </div>
+
+
+
+
+    </body>
+
+
+      
+    <!-- Scripts -->
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <script src="assets/js/isotope.min.js"></script>
+    <script src="assets/js/owl-carousel.js"></script>
+    <script src="assets/js/lightbox.js"></script>
+    <script src="assets/js/custom.js"></script>
+    <script>
+      //according to loftblog tut
+      $(".main-menu li:first").addClass("active");
+
+      var showSection = function showSection(section, isAnimate) {
+        var direction = section.replace(/#/, ""),
+          reqSection = $(".section").filter(
+            '[data-section="' + direction + '"]'
+          ),
+          reqSectionPos = reqSection.offset().top - 0;
+
+        if (isAnimate) {
+          $("body, html").animate(
+            {
+              scrollTop: reqSectionPos
+            },
+            800
+          );
+        } else {
+          $("body, html").scrollTop(reqSectionPos);
+        }
+      };
+
+      var checkSection = function checkSection() {
+        $(".section").each(function() {
+          var $this = $(this),
+            topEdge = $this.offset().top - 80,
+            bottomEdge = topEdge + $this.height(),
+            wScroll = $(window).scrollTop();
+          if (topEdge < wScroll && bottomEdge > wScroll) {
+            var currentId = $this.data("section"),
+              reqLink = $("a").filter("[href*=\\#" + currentId + "]");
+            reqLink
+              .closest("li")
+              .addClass("active")
+              .siblings()
+              .removeClass("active");
+          }
+        });
+      };
+
+      $(".main-menu").on("click", "a", function(e) {
+        e.preventDefault();
+        showSection($(this).attr("href"), true);
+      });
+
+      $(window).scroll(function() {
+        checkSection();
+      });
+    </script>
+  </body>
+</html>
